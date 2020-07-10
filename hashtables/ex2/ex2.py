0196159827efef19e7fc1551db7da58c6bb49bc0
Jon Haase <haase1020@gmail.com>
2020-07-10 13:58:54 -0400

    complete ex5 and tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,37 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    route = [None] * length
+
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+
+    next = "NONE"
+    for x in range(length):
+        if x == 0:
+            route[x] = cache[next]
+            next = cache[route[x]]
+            continue
+
+        route[x] = next
+        next = cache[route[x]]
 
     return route
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+           ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets, 4))
